0ed1e42501dc62961c424fde15c527cda6dae67b
Szymii <szymimelzer9@gmail.com>
2024-04-13 19:04:49 +0200

    Init

---
+++ readme.md
@@ -0,0 +1,7 @@
+### Run
+
+To run it on linux use
+
+```bash
+$ NODE_ENV=development vite build --watch
+```
---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import hotReloadExtension from "hot-reload-extension-vite";
+
+export default {
+  plugins: [
+    hotReloadExtension({
+      log: true,
+      backgroundPath: "src/main.ts", // relative path to background script file
+    }),
+  ],
+};

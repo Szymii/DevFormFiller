b99493816169b5b829d5d776e245faf87ea2b42f
Szymii <szymimelzer9@gmail.com>
2024-04-20 09:27:50 +0200

    Routing

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import HomePage from './pages/HomePage.vue';
+import FormPage from './pages/FormPage.vue';
+
+type RoutesType = keyof typeof routes;
+
+const routes = {
+  '/': HomePage,
+  '/form': FormPage
+};
+
+const currentPath = ref<RoutesType>(window.location.hash as RoutesType);
+
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash as RoutesType;
+});
+
+const currentView = computed(() => {
+  const currentRoute = (currentPath.value.slice(1) || '/') as RoutesType;
+  return routes[currentRoute];
+});
+</script>
 
 <template>
-  <div class="text-3xl font-bold underline">vue test</div>
+  <component :is="currentView" />
 </template>
--- src/pages/FormPage.vue
+++ src/pages/FormPage.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>Home</div>
+  <div>Form</div>
 </template>

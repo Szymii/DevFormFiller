d4ad4b48611b8334f9046b2d91046a30433624b7
Szymii <szymimelzer9@gmail.com>
2024-05-02 19:44:22 +0200

    Load mocks in development env

--- src/app/getForms.ts
+++ src/app/getForms.ts
@@ -1,5 +1,16 @@
 import { forms } from '@/utils/mocks/forms.mock';
+import type { Form } from './types/Form';
 
 export const getForms = () => {
-  return forms;
+  const raw = localStorage.getItem('forms');
+  if (import.meta.env.MODE === 'development' && !raw) {
+    return forms;
+  }
+
+  if (raw) {
+    const localForms = JSON.parse(raw);
+    return localForms as Array<Form>;
+  }
+
+  return [] as Array<Form>;
 };
